b0d2c9f194b7f382d80a65cc505e822c3470eb39
luobin <1096422441@qq.com>
2020-02-28 10:11:42 +0800

    完成Types组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -10,38 +10,17 @@
 </template>
 
 <script lang="ts">
-// export default {
-//   name: 'Types',
-//   data () {
-//     return {
-//       type: '-' // ‘-’表示支出，‘+’号表示收入
-//     }
-//   },
-//   methods: {
-//     selectType(type:string) {
-//       if(type !== '-' && type !== '+') {
-//         throw new Error('type is unknown');
-//       }
-//       this.type = type;
-//     }
-//   }
-// }
 import Vue from 'vue';
 import {Component, Prop} from 'vue-property-decorator';
 
 @Component
 export default class Types extends Vue {
   type:string = '-' ;// type 只能是‘-’表示支出，‘+’号表示收入
-  @Prop(Number) xxx: number | undefined; //任何时候都需要检查xxx有可能是undefined的时候
-  //Number是Vue加的，后面的undefined用于给xxx一个初始值
   selectType(type:string) {
       if(type !== '-' && type !== '+') {
         throw new Error('type is unknown');
       }
       this.type = type;
-  }
-  mounted(): void {
-
   }
 }
 </script>
